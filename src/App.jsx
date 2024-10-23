9d34d5c3f2bbfc4d998ec310636db50401bceeaa
Nathan <yeenathan21@gmail.com>
2024-10-22 21:05:15 -0700

    routing implemented & sample page

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
-import './App.css'
+import { Link } from "react-router-dom"
 
 function App() {
 
   return (
     <>
       <h1>Final Proj</h1>
+      <Link to={"/test"}>Test page</Link>
     </>
   )
 }
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,27 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { Router } from 'react-router-dom';
 import App from './App.jsx'
+import TestPage from "./testpage.jsx"
 import './index.css'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+  },
+  {
+    path: "/test",
+    element: <TestPage/>
+  }
+]);
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>,
 )
