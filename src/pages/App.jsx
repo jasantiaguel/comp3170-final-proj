c33308525c0b53044690a5ab4aeef908967508d8
jasantiaguel <jsantiaguel2@my.bcit.ca>
2024-10-22 23:03:18 -0700

    fixing image render for homepage

--- src/pages/App.jsx
+++ src/pages/App.jsx
@@ -1,10 +1,13 @@
 import React from 'react';
 import WelcomeCard from '../components/WelcomeCard.jsx';
 import '../css/App.css';
+import '../css/index.css';
+import bgImage from "../../public/Background.jpg";
 
 export default function App() {
   return (
     <>
+      <img className="backgroundImage" src={bgImage} alt="vancouver skyline background" />
       <div className="welcome-bg">
         <WelcomeCard />
       </div>
