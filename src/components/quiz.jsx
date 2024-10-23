ed4dd22332c84988dec083bcc1939d5abeb91ba6
Nathan <yeenathan21@gmail.com>
2024-10-22 23:01:24 -0700

    importing images first (vercel fix)

--- src/components/quiz.jsx
+++ src/components/quiz.jsx
@@ -2,6 +2,12 @@ import { useState } from "react"
 import "../css/Quiz.css"
 import { useNavigate } from "react-router-dom";
 
+import img1 from "/public/good-eats/1.png";
+import img2 from "/public/good-eats/2.png";
+import img3 from "/public/good-eats/3.png";
+
+const images = [img1, img2, img3];
+
 const ExpandingMap = () => {
     return(
         <div className="map"/>
@@ -9,10 +15,10 @@ const ExpandingMap = () => {
 }
 
 export default function Quiz(props) {
-    const [number, setNumber] = useState(1);
+    const [number, setNumber] = useState(0);
     const navigate = useNavigate();
     function handleClick() {
-        if (number < 3) {
+        if (number < 2) {
             setNumber(number + 1);
         }
         else {
@@ -23,7 +29,7 @@ export default function Quiz(props) {
     return(
         <>
             <ExpandingMap/>
-            <img className="mainImage" src={`/public/${props.category}/${number}.png`}/>
+            <img className="mainImage" src={images[number]}/>
             <button onClick={handleClick} className="nextButton">Next</button>
         </>
     )
