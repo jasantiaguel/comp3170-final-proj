e8bc2ad4bdc0c831fe19cec28bc53eb70178ad95
jasantiaguel <jsantiaguel2@my.bcit.ca>
2024-12-11 22:40:11 -0800

    feedback modal

--- src/components/quiz.jsx
+++ src/components/quiz.jsx
@@ -4,31 +4,37 @@ import { useNavigate } from "react-router-dom";
 import ExpandingMap from "./ExpandingMap";
 import locations from "../data/locations";
 
-export default function Quiz({category}) {
-    const [number, setNumber] = useState(0);
-    const currentLocation = locations[category][number];
+export default function Quiz({ category }) {
+  const [number, setNumber] = useState(0);
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const currentLocation = locations[category][number];
 
-    const navigate = useNavigate();
-    function handleClick() {
-        if (number < locations[category].length-1) {
-          setScore(score + getScore());
-          setNumber(number + 1);
-        }
-        else {
-          const _result = {category: category, score: score + getScore()};
-          let _newResults = [];
-          if (sessionStorage.getItem("results")) {
-            _newResults = [...JSON.parse(sessionStorage.getItem("results")), _result];
-          } else _newResults = [_result];
-          sessionStorage.setItem("results", JSON.stringify(_newResults));
-          navigate("/results", {state: {result: _result}});
-        }
+  const navigate = useNavigate();
+  function handleClick() {
+    setIsModalOpen(true);
+    if (number < locations[category].length - 1) {
+      setScore(score + getScore());
+      setNumber(number + 1);
+    } else {
+      const _result = { category: category, score: score + getScore() };
+      let _newResults = [];
+      if (sessionStorage.getItem("results")) {
+        _newResults = [
+          ...JSON.parse(sessionStorage.getItem("results")),
+          _result,
+        ];
+      } else _newResults = [_result];
+      sessionStorage.setItem("results", JSON.stringify(_newResults));
+      navigate("/results", { state: { result: _result } });
     }
+  }
 
-    const [coords, setCoords] = useState({});
-    const [targetCoords, setTargetCoords] = useState(currentLocation.targetCoords);
-    const diff = getDiff(targetCoords, coords);
-    const [score, setScore] = useState(0);
+  const [coords, setCoords] = useState({});
+  const [targetCoords, setTargetCoords] = useState(
+    currentLocation.targetCoords
+  );
+  const diff = getDiff(targetCoords, coords);
+  const [score, setScore] = useState(0);
 
   function getDiff(a, b) {
     let lat = a.lat - b.lat;
@@ -39,37 +45,48 @@ export default function Quiz({category}) {
   function getScore() {
     const _lat = Math.abs(diff.lat);
     const _lng = Math.abs(diff.lng);
-    if (_lat < .01 && _lng < .01) {
+    if (_lat < 0.01 && _lng < 0.01) {
       return 10;
-    } else if (_lat < .02 && _lng < .02) {
+    } else if (_lat < 0.02 && _lng < 0.02) {
       return 9;
-    } else if (_lat < .03 && _lng < .03) {
+    } else if (_lat < 0.03 && _lng < 0.03) {
       return 8;
-    } else if (_lat < .04 && _lng < .04) {
+    } else if (_lat < 0.04 && _lng < 0.04) {
       return 7;
-    } else if (_lat < .05 && _lng < .05) {
+    } else if (_lat < 0.05 && _lng < 0.05) {
       return 6;
-    } else if (_lat < .06 && _lng < .06) {
+    } else if (_lat < 0.06 && _lng < 0.06) {
       return 5;
-    } else if (_lat < .07 && _lng < .07) {
+    } else if (_lat < 0.07 && _lng < 0.07) {
       return 4;
-    } else if (_lat < .08 && _lng < .08) {
+    } else if (_lat < 0.08 && _lng < 0.08) {
       return 3;
-    } else if (_lat < .09 && _lng < .09) {
+    } else if (_lat < 0.09 && _lng < 0.09) {
       return 2;
-    } else if (_lat < .1 && _lng < .1) {
+    } else if (_lat < 0.1 && _lng < 0.1) {
       return 1;
     } else return 0;
   }
 
   return (
     <>
-      <ExpandingMap setCoords={setCoords}/>
+      <ExpandingMap setCoords={setCoords} />
       <img
         className="mainImage"
         src={currentLocation.image}
         alt={currentLocation.name}
       />
+      {isModalOpen && (
+        <div className="modal">
+          <div className="modal-content">
+            <span className="close" onClick={() => setIsModalOpen(false)}>
+              &times;
+            </span>
+            <p>Nice guess! 🤔/10</p>
+            <p>Keep going until the end to reveal your score out of 30!</p>
+          </div>
+        </div>
+      )}
       {/* <div
         style={{
           backgroundColor: "white",
